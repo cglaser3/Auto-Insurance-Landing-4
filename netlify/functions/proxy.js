ea5f824a96db6718a08d4eb02f9934adf34070b0
cglaser3 <cglaser3@gmail.com>
2025-07-11 12:14:32 -0400

    Update proxy.js

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,17 +1,7 @@
-const axios = require('axios');
-
-exports.handler = async (event) => {
-  try {
-    const { url } = event.queryStringParameters;
-    const response = await axios.get(url);
-    return {
-      statusCode: 200,
-      body: JSON.stringify(response.data),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Failed to fetch data' }),
-    };
-  }
-};
+[build]
+  command = "npm run build:css"
+  publish = "."
+[[redirects]]
+  from = "/*"
+  to = "/index.html"
+  status = 200
